7a0b643d3737484a103d2002bd374a4d721af03b
eduardz1 <occhipinti.eduard@icloud.com>
2021-12-13 09:36:16 +0100

    comments

--- Users.c
+++ Users.c
@@ -1,13 +1,11 @@
 #define SO_BUDGET_INIT
 #define SO_REWARD
+
+// NON active wait, the time is equivalent to the verification algorithms that happen in "real" blockchains
 #define SO_MIN_TRANS_GEN_NSEC
 #define SO_MAX_TRANS_GEN_NSEC
-#define SO_RETRY 
-void BUDGET(SO_BUDGET_INIT){
-    
-     
 
-
-
+#define SO_RETRY
+void BUDGET(SO_BUDGET_INIT)
+{
 }
-
