980bc5ea15cc04699626fd7822b9c4c0f9f79fed
Edmi6163 <francesco.mauro01@gmail.com>
2021-12-18 23:51:06 +0100

    attempt to implement the timestamp function

--- clock.c
+++ clock.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <time.h>
 
-/* 
- * This is the correct implementation to get a precise timestamp 
- * for the transactions, lesson 2021_12_13 SO T4, prof Druetto 
- */
 
-int main()
-{
-    struct timespec tp; /* need to divide seconds and nanoseconds because it wouldnt fit in a long otherwise */
+/*
+  This is the correct implementation to get a precise timestamp
+ for the transactions, lesson 2021_12_13 SO T4, prof Druetto
+*/
+
+int tracktime(){
+    struct timespec tp; // need to divide seconds and nanoseconds because it wouldn't fit in a long otherwise
 
     clock_gettime(CLOCK_MONOTONIC, &tp);
     printf("Monotonic time: %10ld.%10ld\n", tp.tv_sec, tp.tv_nsec);
@@ -18,5 +18,10 @@ int main()
     clock_gettime(CLOCK_REALTIME, &tp);
     printf("Real time: %10ld.%10ld\n", tp.tv_sec, tp.tv_nsec);
 
-    return 0;
+  return 0;
 }
+/*
+typedef struct timestamp {
+    long long CLOCK_REALTIME;
+}timestamp;
+*/
