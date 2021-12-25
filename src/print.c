4f9de5d8114ddfaecd1d75677fe4146bd77330f1
eduardz1 <occhipinti.eduard@icloud.com>
2021-12-25 12:20:04 +0100

    added print function that need to be defiend

--- src/print.c
+++ src/print.c
@@ -1,6 +1,7 @@
 #include "include/print.h"
 // #define HYPHEN "--------"
 
+/* idk why it's broken */
 void print_user_nodes_table(mainPID, userPID, nodePID, userNum, nodesNum)
 {
     printf("-------- Master Process PID is %d --------\n", mainPID);
@@ -15,3 +16,10 @@ void print_user_nodes_table(mainPID, userPID, nodePID, userNum, nodesNum)
         printf("Node %d\n", nodePID[&nodesNum]);
     }
 }
+
+void print_kill_signal(); 
+void print_user_balance();
+void print_node_balance();
+void print_num_aborted(); 
+void print_num_blocks(); 
+void print_transactions_still_in_pool(); 
