f90e9dd76b45efd0245b0adca268ec4baa4691d5
Eduard <occhipinti.eduard@icloud.com>
2021-12-30 19:48:43 +0100

    files not needed anymore or implemented elsewhere

--- src/include/reward.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int reward(int * transaction_amount, int * reward);
--- src/trash.c
+++
@@ -1,7 +0,0 @@
-typedef struct{
-    int timestamp;
-    int sender;
-    int receiver;
-    int quantity;
-    int reward;
-} transaction;
