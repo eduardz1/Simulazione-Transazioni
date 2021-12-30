bf1af76480507a1cffdd2251474fbc97240bf740
Eduard <occhipinti.eduard@icloud.com>
2021-12-30 19:45:41 +0100

    changed method in which we pass parameters

--- src/master.c
+++ src/master.c
@@ -1,6 +1,51 @@
+#include "include/common.h"
 #include "include/master.h"
 
-pid_t spawn_user(userArgv, shmID)
+/* -- CL PARAMETERS INITIALIZATION -- */
+#define SO_USER_NUM (atoi(argv[1]))
+#define SO_NODES_NUM (atoi(argv[2]))
+#define SO_NUM_FRIENDS (atoi(argv[3]))
+#define SO_SIM_SEC (atoi(argv[4]))
+
+/* -- USER CL PARAMETERS -- */
+#define SO_BUDGET_INIT (argv[5])
+#define SO_REWARD (argv[6])
+#define SO_RETRY (argv[7])
+#define SO_MIN_TRANS_GEN_NSEC (argv[8])
+#define SO_MAX_TRANS_GEN_NSEC (argv[9])
+
+/* -- NODE CL PARAMETERS -- */
+#define SO_TP_SIZE (argv[10])
+#define SO_BLOCK_SIZE (argv[11])
+#define SO_MIN_TRANS_PROC_NSEC (argv[12])
+#define SO_MAX_TRANS_PROC_NSEC (argv[13])
+
+char **user_CL_parameters(char *argv[])
+{
+    char *userArgv[6];
+
+    userArgv[0] = SO_BUDGET_INIT;
+    userArgv[1] = SO_REWARD;
+    userArgv[2] = SO_RETRY;
+    userArgv[3] = SO_MIN_TRANS_GEN_NSEC;
+    userArgv[4] = SO_MAX_TRANS_GEN_NSEC;
+
+    return userArgv;
+}
+
+char **node_CL_parameters(char *argv[])
+{
+    char *nodeArgv[4];
+
+    nodeArgv[0] = SO_TP_SIZE;
+    nodeArgv[1] = SO_BLOCK_SIZE;
+    nodeArgv[2] = SO_MIN_TRANS_PROC_NSEC;
+    nodeArgv[3] = SO_MAX_TRANS_PROC_NSEC;
+
+    return nodeArgv;
+}
+
+pid_t spawn_user(char *userArgv[], int shmID)
 {
     pid_t myPID = fork();
     switch (myPID)
@@ -18,7 +63,7 @@ pid_t spawn_user(userArgv, shmID)
     }
 }
 
-pid_t spawn_node(nodeArgv, shmID)
+pid_t spawn_node(char *nodeArgv[], int shmID)
 {
     pid_t myPID = fork();
     switch (myPID)
@@ -38,96 +83,58 @@ pid_t spawn_node(nodeArgv, shmID)
 
 void interrupt_handle(signum)
 {
-    shmctl(shmID, IPC_RMID, NULL);
+    /* shmctl(shmID, IPC_RMID, NULL); */
 }
 
-int main(int argc, char *argv[]) /* 
-								  *  [1] unsigned int SO_USER_NUM
-								  *  [2] unsigned int SO_NODES_NUM
-								  *  [3] unsigned int SO_NUM_FRIENDS
-								  *  [4] unsigned int SO_SIM_SEC
-                                  *  -------------------------------
-                                  *  [5] unsigned int SO_BUDGET_INIT
-								  *  [6] unsigned int SO_REWARD
-								  *  [7] unsigned int SO_RETRY
-								  *  [8] unsigned long SO_MIN_TRANS_GEN_NSEC
-								  *  [9] unsigned long SO_MAX_TRANS_GEN_NSEC
-								  *  -------------------------------
-								  *  [10]
-								  *  [11]
-								  */
+int main(int argc, char *argv[])
 {
-    int u, n; /* u counter for user processes, n counter for node processes */
-    int shmID; /* ID of "ledger" shared memory segment */
-    unsigned int userNum, nodesNum, friendsNum, simTime;
+    int shmID = 0; /* dummy value */
+    int uCounter, nCounter;
+    unsigned int simTime;
+
     pid_t myPID = getpid();
     pid_t myPPID;
 
-    char *userArgv[5];
-    char *nodeArgv[2];
-
-    struct ledger ledger;
     struct sigaction sa; /* we need to define an handler for CTRL-C command that closes any IPC object */
 
-    /* -- CL PARAMETERS INITIALIZATION -- */
-    userNum = atoi(argv[1]);
-    nodesNum = atoi(argv[2]);
-    friendsNum = atoi(argv[3]);
-    simTime = atoi(argv[4]);
-
     /* -- PID ARRAYS -- */
-    pid_t arrayUserPID[userNum];
-    pid_t arrayNodesPID[nodesNum];
+    pid_t *arrayUserPID = malloc(SO_USER_NUM * sizeof(pid_t));
+    pid_t *arrayNodesPID = malloc(SO_NODES_NUM * sizeof(pid_t));
 
-    /* -- USER CL PARAMETERS -- */
-    userArgv[0] = argv[5]; /* SO_BUDGET_INIT */
-    userArgv[1] = argv[6]; /* SO_REWARD */
-    userArgv[2] = argv[7]; /* SO_RETRY */
-    userArgv[3] = argv[8]; /* SO_MIN_TRANS_GEN_NSEC */
-    userArgv[4] = argv[9]; /* SO_MAX_TRANS_GEN_NSEC */
+    simTime = SO_SIM_SEC;
 
-    /* -- NODE CL PARAMETERS -- */
-    nodeArgv[0] = argv[10];
-    nodeArgv[1] = argv[11];
-
-    /* -- LEDGER INITIALIZATION -- (still needs the code to initialize it)
-     * save the ID of our new (IPC_PRIVATE) shared memory segment of size -ledger- 
-     * smctl will deallocate the shared memory segment only when every process detaches it
-     * tells OS that ledger of type ledger is our shared memory of shmID
-     */
-    shmID = shmget(IPC_PRIVATE, sizeof(*ledger), 0600);
-    shmctl(shmID, IPC_RMID, NULL);
-    ledger = shmat(shmID, NULL, 0);
+    ledger_init();
 
     /* -- SIGNAL HANDLER --
-	 * first set all bytes of sigation to 0
-	 * then initialize sa.handler to a pointer to the function interrupt_handle
-	 * then set the handler to handle SIGINT signals ((struct sigaction *oldact) = NULL) 
-	 */
+     * first set all bytes of sigation to 0
+     * then initialize sa.handler to a pointer to the function interrupt_handle
+     * then set the handler to handle SIGINT signals ((struct sigaction *oldact) = NULL)
+     */
     bzero(&sa, sizeof(sa));
     sa.sa_handler = interrupt_handle;
     sigaction(SIGINT, &sa, NULL);
-    
 
-    for (u; u < userNum; u++)
+    for (uCounter; uCounter < SO_USER_NUM; uCounter++)
     {
-        arrayUserPID[u] = spawn_user(userArgv, shmID);
+        arrayUserPID[uCounter] = spawn_user(user_CL_parameters(argv), shmID);
     }
 
-    for (n; n < nodesNum; n++)
+    for (nCounter; nCounter < SO_NODES_NUM; nCounter++)
     {
-        arrayNodesPID[n] = spawn_node(nodeArgv, shmID);
+        arrayNodesPID[nCounter] = spawn_node(node_CL_parameters(argv), shmID);
     }
 
     wait(simTime);
     kill(mypid(), SIGINT); /* our sigint handler needs to do quite a lot of things to print the wall of test below */
 
-    print_user_nodes_table(myPID, arrayUserPID, arrayNodesPID, userNum, nodesNum);
-    print_kill_signal(); /* need to define, prints reason of termination (simTime elapsed/ledger full/every process terminated) */
-    print_user_balance(); /* need to define, prints balance of every user */
-    print_node_balance(); /* need to define, prints balance of every node */
-    print_num_aborted(); /* need to define, prints num of processes aborted */
-    print_num_blocks(); /* need to define, prints num of blocks saved in the ledger */
+    print_user_nodes_table(myPID, arrayUserPID, arrayNodesPID, SO_USER_NUM, SO_NODES_NUM);
+    print_kill_signal();                /* need to define, prints reason of termination (simTime elapsed/
+                                                                                         ledger full/
+                                                                                         all processes terminated) */
+    print_user_balance();               /* need to define, prints balance of every user */
+    print_node_balance();               /* need to define, prints balance of every node */
+    print_num_aborted();                /* need to define, prints num of processes aborted */
+    print_num_blocks();                 /* need to define, prints num of blocks saved in the ledger */
     print_transactions_still_in_pool(); /* need to define, prints num of transactions still in the pool of each node */
 
     return 0;
