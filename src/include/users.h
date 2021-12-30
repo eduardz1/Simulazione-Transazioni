1c14da26f14b275ab9f9ba4d6e74bf68f9bf1968
Eduard <occhipinti.eduard@icloud.com>
2021-12-30 19:48:08 +0100

    no need to have a separate file that defines reward

--- src/include/users.h
+++ src/include/users.h
@@ -2,15 +2,13 @@
 #define SIMULAZIONE_TRANSAZIONI_USERS_H
 
 #include "balance.h"
-#include "reward.h"
 #include "arrayPID.h"
 
 #include <signal.h>
 #include <stdlib.h>
 #include <time.h>
 #include <signal.h>
-
-// void send_transaction();
+#include <math.h>
 
 void user_transactions_handle(int signum);
 
