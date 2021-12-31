40891818c6c23b882dc3ab890e2441b52782889b
Eduard <occhipinti.eduard@icloud.com>
2021-12-31 15:46:11 +0100

    balance.h and arrayPID.h not needed

--- src/include/users.h
+++ src/include/users.h
@@ -1,9 +1,6 @@
 #ifndef SIMULAZIONE_TRANSAZIONI_USERS_H
 #define SIMULAZIONE_TRANSAZIONI_USERS_H
 
-#include "balance.h"
-#include "arrayPID.h"
-
 #include <signal.h>
 #include <stdlib.h>
 #include <time.h>
