17d8884b341a9fcd500323fbcc08fec65c931b97
Rick <riccardo.oro@gmail.com>
2022-02-18 15:47:36 +0100

    folder

---
+++ Progetto2.0/source/Master.c
@@ -0,0 +1,51 @@
+
+#include <Master.h>
+
+
+#define USER_NAME
+#define NODE_NAME
+
+
+
+
+
+
+
+
+
+
+
+
+void ProcInit(){
+     pid_t Parent; 
+     pid_t Child; 
+     Parent=getppid();
+     Child=getpid(); 
+
+}
+
+
+
+struct Transaction  
+{
+    timestamp; 
+    reciver; 
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+     
+
+
+}
