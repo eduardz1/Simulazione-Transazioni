f9d63bbc539086c89fd88fd2aa58305d1832dba1
Edmi6163 <francesco.mauro01@gmail.com>
2022-02-21 18:38:15 +0100

    Added CTRL-C handler

--- Progetto2.0/source/Master.c
+++ Progetto2.0/source/Master.c
@@ -1,15 +1,15 @@
-#include <Master.h>
+#include "include/Master.h" 
 
 
 
 
 
-
+/*
 void Shared_Memory( key_t key,size_t size,int shmflg){  
      int m_id; 
      int Init=shmget(IPC_PRIVATE,sizeof(SO_REGISTRY_SIZE)*2,0666); //ShdMem Define Area 
-     /*struct Shared_Data = shmat(m_id,NULL,0); */  
-}
+     /*struct Shared_Data = shmat(m_id,NULL,0);   
+} */
 
 
 
@@ -34,9 +34,20 @@ void NodeInit(){
 
 
 /* Stop Simulation handler Ctrl-C */
-void master_Stop_handle(){
-    int status,WPid; 
-    signal(SIGINT,/* FUNZIONE DA TERMINARE */); 
+void master_Stop_handler(int sigum){
+     char c;
+     signal(sigum,SIG_IGN);
+     printf("did you pressed CTRL-C? \n" "would you like to quit?[y/n]");
+     c=getchar();
+
+     if(c=='y'||c=='Y'){
+          exit(0);
+     } else {
+          signal(SIGINT,master_Stop_handler);
+          getchar();
+     }
+
+
      
 }
 
@@ -45,7 +56,11 @@ void master_Stop_handle(){
 
 
 int main(){
-     
+int i;
+     signal(SIGINT,master_Stop_handler);
+     UsersInit();
+     NodeInit();
+for(i=0;;i++){
 
-
+}
 }
--- Progetto2.0/source/include/Master.h
+++ Progetto2.0/source/include/Master.h
@@ -1,47 +1,14 @@
-#define _GNU_SOURCE
-
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <signal.h>
-#define SO_REGISTRY_SIZE 
-#define USER_NAME
-#define NODE_NAME
-#define SO_USERS_NUM 
-#define SO_NODES_NUM 
+#include <signal.h>   /* for signal handler*/
+#include <stdlib.h> /*for exit function*/
 
-
-void Shared_Memory( key_t key,size_t size,int shmflg); 
-void UsersInit(); 
-void UserInit(); 
-void NodeInit(); 
-void master_Stop_handle(); 
-
-struct Shared_Data
-{
-    unsigned long indx_write; 
-    
-    pid_t Pid_UserSHM[SO_USERS_NUM];
-     
-};
-
-
-
-
-
+void master_Stop_handler(int sigum);
+/*
 struct Transaction  
 {
     timestamp; 
     reciver; 
-    MoneySend; 
-    Reward; 
-};
-
-
-
-
-
-
-
+    
+}; */
