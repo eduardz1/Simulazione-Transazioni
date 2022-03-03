57362c96dddc16d5fc2d402aa9756e66f69e6722
Edmi6163 <francesco.mauro01@gmail.com>
2022-03-03 11:33:24 +0100

    small changes

--- Progetto2.0/source/Print.c
+++ Progetto2.0/source/Print.c
@@ -1,11 +1,13 @@
 #include <pthread.h>
+#include "include/Common.h"
 void *printing(){
-
+	printf("i'm a thread\n");
 }
 
-/*thread initialization according to https://www.geeksforgeeks.org/multithreading-c-2/*/
+/*thread initialization according to https://www.geeksforgeeks.org/multithreading-c-2/ */
 int main(){
 pthread_t threadId;
 pthread_create(&threadId,NULL,printing,NULL);
 pthread_join(threadId,NULL);
+printf("i'm out of thread\n");
 }
