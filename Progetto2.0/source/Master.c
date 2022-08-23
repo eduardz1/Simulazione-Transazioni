c54624aa127f4f466a4fa9ae62bd943ac505f0d9
Francesco Mauro <mauro.francesco@educ.di.unito.it>
2022-08-23 16:58:59 +0200

    now master compile

--- Progetto2.0/source/Master.c
+++ Progetto2.0/source/Master.c
@@ -11,6 +11,41 @@ Block_ ledger[SO_REGISTRY_SIZE];
 Block_ *tmpLedger;
 int mQueue;
 
+
+int receive_message(int queueID,void *msg,int size,int mtype,int flag){
+  if(msgrcv(queueID,msg,size,mtype,flag)==0){
+    return 0; /*SUCCESS*/
+  }
+  switch (errno)
+  {
+    case EACCES:
+      fprintf(stderr,"[MSG SEND %d] no write permission on queue\n", getpid());
+      break;
+    case EAGAIN:
+      fprintf(stderr,"[MSG SEND %d] couldn't write on queue\n", getpid());
+      break;
+    case EFAULT:
+      fprintf(stderr,"[MSG SEND %d] address pointed by msgp inaccessible\n", getpid());
+      break;
+    case EIDRM:
+      fprintf(stderr,"[MSG SEND %d] message queue removed\n", getpid());
+      break;
+    case EINTR:
+      fprintf(stderr,"[MSG SEND %d] signal caught when waiting for queue to free\n", getpid());
+      break;
+    case EINVAL:
+      fprintf(stderr,"[MSG SEND %d] invalid  msqid  value,  or non positive mtype value, or invalid msgs value\n", getpid());
+      break;
+    case ENOMEM:
+      fprintf(stderr,"[MSG SEND %d] system out of memory\n", getpid()); /* hoping this never happens */
+      break;
+    default:
+      return 0;
+      break;
+  }
+  return -1;
+}
+
 int message_queue_id()
 {
 	int pidGot = getpid();
@@ -146,7 +181,7 @@ int main()
 
 	mQueue = message_queue_id();
 
-	for (nCounter = 0; nCounter < SO_NODES_NUM; nCounter++)
+	for (nCounter = 0; nCounter < SO_NODES_NUM; nCounter++) /*TODO: seg fault here imo, need to solve */
 	{
 		nodesPid[nCounter].Node_state = ALIVE;
 		nodesPid[nCounter].balance = 0;
