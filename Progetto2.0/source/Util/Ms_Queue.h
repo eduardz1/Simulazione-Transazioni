ce3f7c88eef387c55ba1c0c2f07757d67eb9136e
Francesco Mauro <mauro.francesco@educ.di.unito.it>
2022-09-21 04:57:18 +0200

    fix file not found

--- Progetto2.0/source/Util/Ms_Queue.h
+++ Progetto2.0/source/Util/Ms_Queue.h
@@ -8,7 +8,7 @@ const void *Message_p;
 size_t Message_size; 
  
 /*PROTOTYPE'S */
-void Message_Queue();
+/*void Message_Queue();*/
 
 int send_message(int queueID,void *msg,int size,int flag);
 int receive_message(int queueID,void *msg,int size,int mtype,int flag);
